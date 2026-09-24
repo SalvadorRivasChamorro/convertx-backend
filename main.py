86c125f5938c7def39e29f308d0c6aea4037d80a
SalvadorRivasChamorro <salvador.rivas.chamorro@gmail.com>
2026-09-24 13:59:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="ConvertX API")
 
+
+# Permitir conexión desde Cloudflare
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 def home():
-    return {"status":"ConvertX API funcionando"}
+    return {
+        "status": "ConvertX API funcionando"
+    }
