d69bb810230a54a6feb8cefd5de6ec1a6bd6834e
SalvadorRivasChamorro <salvador.rivas.chamorro@gmail.com>
2026-09-24 14:04:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from typing import List
+import tempfile
+import os
+import zipfile
+
 
 app = FastAPI(title="ConvertX API")
 
 
-# Permitir conexión desde Cloudflare
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -19,3 +24,29 @@ def home():
     return {
         "status": "ConvertX API funcionando"
     }
+
+
+@app.post("/convert")
+async def convert(
+    files: List[UploadFile] = File(...),
+    type: str = Form(...)
+):
+
+    folder = tempfile.mkdtemp()
+    zip_path = os.path.join(folder, "convertidos.zip")
+
+    with zipfile.ZipFile(zip_path, "w") as zipf:
+
+        for file in files:
+            path = os.path.join(folder, file.filename)
+
+            with open(path, "wb") as f:
+                f.write(await file.read())
+
+            zipf.write(path, file.filename)
+
+
+    return FileResponse(
+        zip_path,
+        filename="convertidos.zip"
+    )
