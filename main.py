ad9de8e3975f20db70723175f0351b497eefa0a8
SalvadorRivasChamorro <salvador.rivas.chamorro@gmail.com>
2026-09-24 14:07:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from typing import List
 import tempfile
 import os
 import zipfile
+import subprocess
 
 
 app = FastAPI(title="ConvertX API")
@@ -21,9 +22,7 @@ app.add_middleware(
 
 @app.get("/")
 def home():
-    return {
-        "status": "ConvertX API funcionando"
-    }
+    return {"status": "ConvertX API funcionando"}
 
 
 @app.post("/convert")
@@ -35,18 +34,63 @@ async def convert(
     folder = tempfile.mkdtemp()
     zip_path = os.path.join(folder, "convertidos.zip")
 
+    converted_files = []
+
+
+    for file in files:
+
+        original = os.path.join(folder, file.filename)
+
+        with open(original, "wb") as f:
+            f.write(await file.read())
+
+
+        output = folder
+
+
+        if type == "docx_pdf":
+            formato = "pdf"
+
+        elif type == "ppt_pdf":
+            formato = "pdf"
+
+        elif type == "pdf_docx":
+            formato = "docx"
+
+        else:
+            formato = "pptx"
+
+
+        subprocess.run([
+            "libreoffice",
+            "--headless",
+            "--convert-to",
+            formato,
+            "--outdir",
+            output,
+            original
+        ])
+
+
+        nombre = os.path.splitext(file.filename)[0] + "." + formato
+
+        convertido = os.path.join(folder, nombre)
+
+
+        if os.path.exists(convertido):
+            converted_files.append(convertido)
+
+
     with zipfile.ZipFile(zip_path, "w") as zipf:
 
-        for file in files:
-            path = os.path.join(folder, file.filename)
-
-            with open(path, "wb") as f:
-                f.write(await file.read())
-
-            zipf.write(path, file.filename)
+        for file in converted_files:
+            zipf.write(
+                file,
+                os.path.basename(file)
+            )
 
 
     return FileResponse(
         zip_path,
-        filename="convertidos.zip"
+        filename="ConvertX_resultados.zip"
     )
